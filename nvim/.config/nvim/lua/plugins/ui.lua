d61c0acb8e1655b2ab19bb5266aadd97b13674a8
Dan Snow <dan@sno.ws>
2023-09-29 16:31:53 -0500

    Bump indent-blankline version

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -62,7 +62,7 @@ return {
       vim.opt.list = true
       vim.opt.listchars:append "eol:↴"
       vim.opt.listchars:append "space:⋅"
-      require("indent_blankline").setup({
+      require("ibl").setup({
         show_current_context = true,
         show_end_of_line = true,
         space_char_blankline = " ",
