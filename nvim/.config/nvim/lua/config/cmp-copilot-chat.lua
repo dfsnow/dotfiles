d6b6f0c3a6834efd165b5b5dcf1170dabc8fdfbb
Dan Snow <dan@sno.ws>
2024-12-05 22:06:03 -0600

    Update Copilot Chat completion source

--- nvim/.config/nvim/lua/config/cmp-copilot-chat.lua
+++ nvim/.config/nvim/lua/config/cmp-copilot-chat.lua
@@ -2,6 +2,56 @@ local cmp = require("cmp")
 local chat = require("CopilotChat")
 local Source = {}
 
+--- Modified completion items that doesn"t load agents/models (increases speed)
+--- https://github.com/CopilotC-Nvim/CopilotChat.nvim/blob/1fe19d1fdbf9edcda8bad9b7b2d5e11aa95c1672/lua/CopilotChat/init.lua#L494
+---@param callback function(table)
+local function complete_items(callback)
+  local prompts_to_use = chat.prompts()
+  local items = {}
+
+  for name, prompt in pairs(prompts_to_use) do
+    local kind = ""
+    local info = ""
+    if prompt.prompt then
+      kind = "user"
+      info = prompt.prompt
+    elseif prompt.system_prompt then
+      kind = "system"
+      info = prompt.system_prompt
+    end
+
+    items[#items + 1] = {
+      word = "/" .. name,
+      kind = kind,
+      info = info,
+      menu = prompt.description or "",
+      icase = 1,
+      dup = 0,
+      empty = 0,
+    }
+  end
+
+  for name, value in pairs(chat.config.contexts) do
+    items[#items + 1] = {
+      word = "#" .. name,
+      kind = "context",
+      menu = value.description or "",
+      icase = 1,
+      dup = 0,
+      empty = 0,
+    }
+  end
+
+  table.sort(items, function(a, b)
+    if a.kind == b.kind then
+      return a.word < b.word
+    end
+    return a.kind < b.kind
+  end)
+
+  callback(items)
+end
+
 function Source:get_trigger_characters()
   return chat.complete_info().triggers
 end
@@ -11,24 +61,24 @@ function Source:get_keyword_pattern()
 end
 
 function Source:complete(params, callback)
-   chat.complete_items(function(items)
-     items = vim.tbl_map(function(item)
-       return {
-         label = item.word,
-         kind = cmp.lsp.CompletionItemKind.Keyword,
-         documentation = item.menu or ""
-       }
-     end, items)
+  complete_items(function(items)
+    items = vim.tbl_map(function(item)
+      return {
+        label = item.word,
+        kind = cmp.lsp.CompletionItemKind.Keyword,
+        documentation = item.menu ~= "" and item.menu or item.info or ""
+      }
+    end, items)
 
-     local prefix = string.lower(params.context.cursor_before_line:sub(params.offset))
+    local prefix = string.lower(params.context.cursor_before_line:sub(params.offset))
 
-     callback({
-       items = vim.tbl_filter(function(item)
-         return vim.startswith(item.label:lower(), prefix:lower())
-       end, items),
-     })
-   end)
- end
+    callback({
+      items = vim.tbl_filter(function(item)
+        return vim.startswith(item.label:lower(), prefix:lower())
+      end, items),
+    })
+  end)
+end
 
 ---@param completion_item lsp.CompletionItem
 ---@param callback fun(completion_item: lsp.CompletionItem|nil)
