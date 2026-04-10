7d73723a045fe9b32e094eb42f0a261ed360ae0a
Dan Snow <dan@sno.ws>
2026-04-10 16:39:43 -0700

    Enable undofile in nvim

--- nvim/.config/nvim/plugin/core.lua
+++ nvim/.config/nvim/plugin/core.lua
@@ -4,33 +4,27 @@ vim.pack.add({
   gh("tpope/vim-sleuth"),
 })
 
--- Setup built-in undotree
-helpers = require("helpers")
-vim.cmd("packadd nvim.undotree")
-vim.keymap.set("n", "<leader>u", function()
-  local w, h, c, r = helpers.get_float_size(
-    float_width_pct,
-    float_height_pct,
-    vim.o.columns
-  )
-
-  local cmd_string = "call nvim_open_win(nvim_create_buf(v:true, v:true), v:true, "
-    .. "{'relative': 'editor', 'width': " .. w
-    .. ", 'height': " .. h
-    .. ", 'col': " .. c
-    .. ", 'row': " .. r
-    .. " })"
-  
-  require("undotree").open({ command = cmd_string, title = "undotree" })
-
-        vim.api.nvim_win_set_config(0, {
-          relative = "editor",
-          border = "single",
-          width = w,
-          height = h,
-          row = r,
-          col = c
-        })
-end, { desc = "Open undotree" })
-
 require("nvim-surround").setup({})
+
+-- Setup built-in undotree
+if not vim.g.vscode then
+  helpers = require("helpers")
+  vim.opt.undofile = true
+  vim.cmd("packadd nvim.undotree")
+  vim.keymap.set("n", "<leader>u", function()
+    local w, h, c, r = helpers.get_float_size(
+      float_width_pct,
+      float_height_pct,
+      vim.o.columns
+    )
+
+    local cmd_string = "call nvim_open_win(nvim_create_buf(v:true, v:true), v:true, "
+      .. "{'relative': 'editor', 'width': " .. w
+      .. ", 'height': " .. h
+      .. ", 'col': " .. c
+      .. ", 'row': " .. r
+      .. " })"
+    
+    require("undotree").open({ command = cmd_string, title = "undotree" })
+  end, { desc = "Open undotree" })
+end
